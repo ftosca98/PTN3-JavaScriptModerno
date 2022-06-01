d01f2339ec131a73c4054ae7a4e21ed4844b102a
ftweb <ftosca98@gmail.com>
2022-06-01 08:01:40 -0300

    fix

--- operador-ternario.js
+++ operador-ternario.js
@@ -1,9 +1,24 @@
 /*punto5: operador condicional ternario:*/
-const alumnos= 0;
+
+
+/*ej1*/
+const AlumnosEnCursada = (x,carrera) => (x > carrera ) ?"se cursa la carrera" : "se cierra la carrera";
+
+console.log(AlumnosEnCursada (20,10));
+
+
+/*ej2*/
+const alumnos= 40;
+const carrera = alumnos >20 ? "se cursa la carrera" : "se cierra la carrera" ; 
+         
+
+console.log({alumnos,carrera});
+
+/*const alumnos= 0;
 let mostrarMensaje;
 (alumnos > 20)
  ? mostrarMensaje = "se cursa la carrera"
 : mostrarMensaje = "se cierra la carrera";
 
-console.log(mostrarMensaje);
+console.log(mostrarMensaje);*/
 
